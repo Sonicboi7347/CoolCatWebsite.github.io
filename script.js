33cc8a4b23e890f4be78a6b69708fbc08e11f716
Sonicboi7347 <102981061+Sonicboi7347@users.noreply.github.com>
2026-06-07 15:22:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const catFacts = [
-    "Cats can jump up to six times their height.",
     "Cats spend about 70% of their lives sleeping.",
-    "A group of cats is called a clowder.",
-    "Cats have over 20 ear muscles.",
     "A cat's nose print is unique like a fingerprint.",
+    "Cats can jump six times their height.",
+    "Cats have over 20 ear muscles.",
     "The oldest known pet cat lived over 9,000 years ago.",
+    "A group of cats is called a clowder.",
     "Cats can rotate their ears 180 degrees.",
-    "Most cats don't have eyelashes."
+    "Some cats can run over 48 km/h.",
+    "Cats have excellent night vision.",
+    "Cats use whiskers to help navigate tight spaces."
+    "I love Cats"
+    "Cats are cool as hell"
 ];
 
 function showFact() {
-    const randomIndex = Math.floor(Math.random() * catFacts.length);
+    const randomFact =
+        catFacts[Math.floor(Math.random() * catFacts.length)];
 
-    document.getElementById("fact").textContent =
-        catFacts[randomIndex];
+    document.getElementById("fact").textContent = randomFact;
 }
